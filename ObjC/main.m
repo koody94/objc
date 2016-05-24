6fc56c1e6e6d382c68a2bf45342439b8d00aab75
doyeon koo <koody94@naver.com>
2016-05-24 16:09:03 +0900

    4.4

--- ObjC/main.m
+++ ObjC/main.m
@@ -66,14 +66,14 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         int a=25;
-        int b=2;
-        float c=25.0;
-        float d=2.0;
+        int b=5;
+        int c=10;
+        int d=7;
        
-        NSLog(@"6+a/5*b=%i",6+a/5*b);
-        NSLog(@"a/b*b=%i",a/b*b);
-        NSLog(@"c/d*d=%f",c/d*d);
-        NSLog(@"-a=%i",-a);
+        NSLog(@"a %% b=%i",a % b);
+        NSLog(@"a %% c=%i",a % c);
+        NSLog(@"a %% d=%i",a % d);
+        NSLog(@"a / d * d + a %% d=%i",a / d * d + a % d);
 
     }
     return 0;
