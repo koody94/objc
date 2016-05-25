4cfff01489f9da698cbe16db94fedf244d987732
doyeon koo <koody94@naver.com>
2016-05-25 12:16:56 +0900

    6.1

--- ObjC/main.m
+++ ObjC/main.m
@@ -11,16 +11,17 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int number, right_digit;
         
-        NSLog(@"Enter your nuber.");
+        int number;
+        
+        NSLog(@"Type in your number: ");
         scanf("%i",&number);
         
-        do{
-            right_digit=number %10;
-            NSLog(@"%i",right_digit);
-            number /=10;
-        }while( number !=0);
+        if(number <0)
+            number = -number;
+        
+        NSLog(@"The absolute value is %i",number);
+
     }
     return 0;
 }
