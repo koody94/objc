2e68f0f0a5d007754c957922f58e43ce8860563b
doyeon koo <koody94@naver.com>
2016-05-25 14:10:56 +0900

    6.7

--- ObjC/main.m
+++ ObjC/main.m
@@ -12,19 +12,19 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        int year, rem_4, rem_100, rem_400;
+        int number, sign;
         
-        NSLog(@"Enter the year to be tested: ");
-        scanf("%i", &year);
+        NSLog(@"Please type in a number:");
+        scanf("%i", &number);
         
-        rem_4=year % 4;
-        rem_100=year %100;
-        rem_400 = year %400;
-        
-        if((rem_4==0 && rem_100!=0)||rem_400==0)
-            NSLog(@"It's a leap year");
+        if(number < 0)
+            sign=-1;
+        else if(number ==0)
+            sign=0;
         else
-            NSLog(@"Nope, it's not a leap year");
+            sign=1;
+        
+        NSLog(@"Sign =%i",sign);
     }
     return 0;
 }
