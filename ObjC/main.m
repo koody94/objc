e1655bd365787c4c95b782eada6eee8e79323af0
doyeon koo <koody94@naver.com>
2016-05-26 14:20:06 +0900

    9.2

--- ObjC/main.m
+++ ObjC/main.m
@@ -19,35 +19,22 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        
+        id dataValue;
+        
         Fraction *f1=[[Fraction alloc]init];
-        Fraction *f2=[[Fraction alloc]init];
-        Fraction *fracResult;
         
         Complex *c1 = [[Complex alloc]init];
-        Complex *c2 = [[Complex alloc]init];
-        Complex *compResult;
         
-        [f1 setTo:1 over:10];
-        [f2 setTo:2 over:15];
+        [f1 setTo:2 over:5];
         
-        [c1 setReal:18.0 andImaginary:2.5];
-        [c2 setReal:5.0 andImaginary:3.2];
+        [c1 setReal:10.0 andImaginary:2.5];
         
-        [c1 print];
-        NSLog(@"        +");
-        [c2 print];
-        NSLog(@"---------");
+        dataValue = f1;
+        [dataValue print];
         
-        compResult = [c1 add:c2];
-        [compResult print];
-        NSLog(@"\n");
-        
-        [f1 print];
-        NSLog(@"   +");
-        [f2 print];
-        NSLog(@"----");
-        fracResult = [f1 add:f2];
-        [fracResult print];
+        dataValue = c1;
+        [dataValue print];
     }
     return 0;
 }
